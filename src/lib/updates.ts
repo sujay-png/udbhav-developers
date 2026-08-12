bee58dd4b3bd0480ee83e13ca8b6cc1f5b009e64
sanjana561 <141301733+sanjana561@users.noreply.github.com>
2026-08-12 13:03:52 +0530

    updated content

---
+++ src/lib/updates.ts
@@ -0,0 +1,76 @@
+export const updates = [
+  {
+    id: 1,
+    title:
+      "Marcel’s Maison inaugurated in Valencia - A new landmark for premium living, business",
+    src: "https://www.daijiworld.com/news/newsDisplay?newsID=1265898",
+    status: "Completed",
+  },
+  {
+    id: 2,
+    title:
+      "Ground-breaking ceremony for Udbhav Developers’ 'Marcel’s Maison' held",
+    src: "https://www.daijiworld.com/news/newsDisplay?newsID=1043758",
+  },
+  {
+    id: 3,
+    title:
+      "Udbhav Developers presents Marcel’s Maison at Valencia - Bookings open",
+    src: "https://www.daijiworld.com/news/newsDisplay?newsID=1042541",
+    status: "Booking",
+  },
+  {
+    id: 4,
+    title:
+      "Udbhav Developers’ 'KRK Aayush' residential building inaugurated",
+    src: "https://www.daijiworld.com/news/newsDisplay?newsID=974722#.Yr8JRfBFhHE.whatsapp",
+  },
+  {
+    id: 5,
+    title:
+      "Exclusive 3BHK residential apartments in Udbhav Shanthi at Kadri - Bookings open",
+    src: "https://www.daijiworld.com/news/newsDisplay?newsID=940244",
+    status: "Booking",
+  },
+  {
+    id: 6,
+    title:
+      "Ready to occupy commercial space available at ‘Udbhav Square’ at Kottara",
+    src: "https://www.daijiworld.com/news/newsDisplay.aspx?newsID=735321",
+    status: "Ready to Occupy",
+  },
+   {
+    id: 7,
+    title: "Mangaluru’s finest commercial complex ‘Udbhav Square’ inaugurated.",
+    src: "https://www.daijiworld.com/news/newsDisplay.aspx?newsID=678408/",
+  },
+  {
+    id: 8,
+    title: "‘Udbhav Square’ in Kottara Chowki - Golden opportunity for investors",
+    src: "https://www.daijiworld.com/news/newsDisplay.aspx?newsID=511578",
+  },
+  {
+    id: 9,
+    title: "Bhaskar Coconut Groves' apartments inaugurated.",
+    src: "https://www.daijiworld.com/news/newsDisplay?newsID=325279",
+  },
+  {
+    id: 10,
+    title:
+      "Foundation stone Laid for Bhaskar Coconut Groves at Near Nantoor Junction, Mangalore",
+    src: "https://www.daijiworld.com/chan/sponsoredDisplay.aspx?sID=3944",
+  },
+  {
+    id: 11,
+    title:
+      "New residential project ‘Udbhav Chinmaya’ launched in heart of Mangaluru",
+    src: "https://daijiworld.com/news/newsDisplay?newsID=1295161",
+  },
+];
+
+export type updates = {
+  id: number;
+  title: string;
+  src: string;
+  status?: string;
+};
