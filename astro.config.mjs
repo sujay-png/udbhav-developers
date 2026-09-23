ca0b37b5e51f273d412c29af248612d9ba8d873c
mohammedraees07 <mohammedraees004@gmail.com>
2026-09-23 16:55:25 +0530

    fix:canonical with leading slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.udbhavdevelopers.com',
-  trailingSlash: 'ignore',
+  trailingSlash: 'always',
   output: 'server', // This explicitly enables Server-Side Rendering
   adapter: vercel(),
   vite: {
